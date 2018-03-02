f3de47ecf056f80bf79ef329af5adfafac30c5a7
Camille Campbell <c.campbell1996@hotmail.com>
2018-03-02 20:44:26 +0000

    Updated the login route

--- app/views.py
+++ app/views.py
@@ -34,19 +34,24 @@ def login():
     if request.method == "POST":
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+        if form.validate_on_submit():
+            username=form.username.data
+            pword = form.password.data
+            user = UserProfile.query.filter_by(username=username, pword=password).first()
+            if user is not None:
+                
             # Get the username and password values from the form.
-
+           
             # using your model, query database for a user based on the username
             # and password submitted
             # store the result of that query to a `user` variable so it can be
             # passed to the login_user() method.
 
             # get user id, load into session
-            login_user(user)
-
+                login_user(user)
+                flash("Login Successful", "success")
             # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+                return redirect(url_for("secure-page"))  # they should be redirected to a secure-page route instead
     return render_template("login.html", form=form)
 
 
